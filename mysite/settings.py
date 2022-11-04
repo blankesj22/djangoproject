736f4f423997e43d3e5ecc3d716465309582a4f2
blankesj22 <blank.esj@gmail.com>
2022-11-03 19:11:11 -0500

    postgres config

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'curso_django',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
