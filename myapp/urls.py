8896aa8163639a23cabfa3db51688674e68d8614
blankesj22 <blank.esj@gmail.com>
2022-11-05 19:46:49 -0500

    segundo commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,22 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello),
-    path('about/', views.about),
+    path('', views.index, name='index'),
+
+    path('about/', views.about, name='about'),
+
+    path('hello/<str:username>', views.hello, name='hello'),
+
+    path('projects/', views.projects, name='projects'),
+    path('create_project/', views.create_project, name='create_project'),
+    path('projects/<int:id>', views.project_detail, name='project_detail'),
+
+    path('tasks/', views.tasks, name='tasks'),
+    path('create_task/', views.create_task,     name='create_task'),
+    
+   
+
+    # return a json response
+    path('json/', views.json1),
+
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,61 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project, Task
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import CreateNewTask, CreateNewProject
+import json
 
 # Create your views here.
-def hello(request):
-    return HttpResponse("<h2>Hello World</h2>")
+
+
+def index(request):
+    title = 'Django Course!!'
+    return render(request, 'index.html', {'title': title})
+
 
 def about(request):
-    return HttpResponse('About')
+    username = 'Edward'
+    return render(request, 'about.html', {'username': username})
+
+
+def hello(request, username):
+    print(username)
+    return HttpResponse("<h2>Hello %s </h2>" % username)
+
+
+def projects(request):
+    projects = list(Project.objects.values())
+    return render(request, 'projects/projects.html', {'projects': projects})
+
+
+def tasks(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/tasks.html', {'tasks': tasks})
+
+
+def create_task(request):
+    if request.method == 'GET':
+        return render(request, 'tasks/create_task.html', {'form': CreateNewTask()})
+    else:
+        Task.objects.create(
+            title=request.POST['title'], description=request.POST['description'], project_id=2)
+        return redirect('tasks')
+
+
+def create_project(request):
+    if request.method == 'GET':
+        return render(request, 'projects/create_project.html', {'form': CreateNewProject()})
+    else:
+        Project.objects.create(name=request.POST['name'])
+        return redirect('projects')
+
+
+def project_detail(request, id):
+    project = get_object_or_404(Project, id=id)
+    tasks = Task.objects.filter(project_id=id)
+    return render(request, 'projects/project_detail.html', {'project': project, 'tasks': tasks})
+
+
+# json response
+def json1(request):
+    data = {'val1': 'this is x', 'val2': True}
+    return HttpResponse(json.dumps(data))
